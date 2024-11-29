f6e1a38d0ac0ef0b8efa7049103cc1cb07d4e7af
ValentinMazurier <valentin.mazu@gmail.com>
2024-11-29 11:00:28 +0100

    Préparation du formulaire pour modification des jeux, enregistrement sur GitHub

--- script.js
+++ script.js
@@ -43,8 +43,23 @@ function writeDom() {
                             Description du jeu</p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    <button type="button" 
+                                    class="btn btn-sm btn-outline-secondary view" 
+                                    data-bs-toggle="modal" 
+                                    data-bs-target="#exampleModal"
+                                    data-edit-id="${game.id}">
+                                    
+
+                                    View</button>
+                                    <button type="button" 
+                                    class="btn btn-sm btn-outline-secondary edit"
+                                    data-edit-id="${game.id}"
+                                    data-bs-toggle="modal" 
+                                    data-bs-target="#exampleModal">
+                                    
+                                    Edit</button>
+
+                                    
                                 </div>
                             </div>
                         </div>
@@ -54,3 +69,70 @@ function writeDom() {
 }
 
 writeDom()
+
+const editButtons = document.querySelectorAll(".edit")
+editButtons.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+
+        console.log(e.target.getAttribute("data-edit-id"))
+		editModal(e.target.getAttribute("data-edit-id"))
+	})
+})
+
+function editModal(gameId) {
+	// console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	modifyModal("Mode Edition")
+}
+
+function modifyModal(modalTitle) {
+	// Écrir le nom du jeu dans le titre du modal
+	document.querySelector(".modal-title").textContent = modalTitle
+}
+
+const viewButtons = document.querySelectorAll(".view")
+viewButtons.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+        console.log(e.target.getAttribute("data-edit-id"))
+		viewModal(e.target.getAttribute("data-edit-id"))
+	})
+})
+
+function viewModal(gameId) {
+	// console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	modifyModal(gamesList[result].title)
+}
+
+function editModal(gameId) {
+	// console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	modifyModal("Mode Edition")
+}
+
+function modifyModal(modalTitle, modalBody) {
+	// Écrir le nom du jeu dans le titre du modal
+	document.querySelector(".modal-title").textContent = modalTitle
+	document.querySelector(".modal-body").innerHTML = modalBody
+}
+
+function viewModal(gameId) {
+	// console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	const modalBody = `<img src="${gamesList[result].imageUrl}" alt="${gamesList[result].title}" class="img-fluid" />`
+	modifyModal(gamesList[result].title, modalBody)
+}
+
+function editModal(gameId) {
+	// console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	// passer une image comme corps du modal
+	const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
+	modifyModal("Mode Edition", modalBody)
+}
+
